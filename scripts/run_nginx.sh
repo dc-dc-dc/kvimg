5f17b480c47fa60662f131ad0c35d374fac57cad
Diogo Da Cruz <dgcruz983@gmail.com>
2022-06-07 16:48:00 -0400

    fixed data permissions

--- scripts/run_nginx.sh
+++ scripts/run_nginx.sh
@@ -6,15 +6,17 @@ source "${SCRIPT_DIR}/setup.sh"
 "${SCRIPT_DIR}/network.sh"
 
 PORT=3000
-VOLUME=${1:-test}
+VOLUME=${1:-vol1}
 C_PATH="${DATA_DIR}/${VOLUME}"
 NAME="nginx_vol_${VOLUME}"
 if [ ! -d ${C_PATH} ]; then
     echo "no dir creating"
+    chmod 777 ${C_PATH}
     mkdir -p ${C_PATH}
 fi
 
 $DOCKER_CMD create \
+    --rm \
     --name ${NAME} \
     -v ${C_PATH}:/data \
     -p ${PORT}:80 \
